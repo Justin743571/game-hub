8d7a4fdbc55ecf2ee610e43d62ece8b4ffc46fab
王旭 <wxu743571@gmail.com>
2024-05-24 13:01:17 +0800

    通过Platform过滤游戏

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/platformSelector";
+import { Platform } from "./hooks/usePlatforms";
 
 function App() {
   const [selectedGenre,setSelectedGenre] = useState<Genre|null>(null);
+  const [selectedPlatform,setSelectedPlatform] = useState<Platform|null>(null);
+
   return (
     <Grid
       templateAreas={{
@@ -28,8 +31,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector/>
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectedPlatform={(platform) => setSelectedPlatform(platform)}/>
+        <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} />
       </GridItem>
     </Grid>
   );
