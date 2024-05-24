3b48fd66a531e710279bc844d3a1f2533219b2b6
王旭 <wxu743571@gmail.com>
2024-05-24 11:21:11 +0800

    展示spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,20 +1,39 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  if (error) return;
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image src={genre.image_background} boxSize="32px" borderRadius={8}/>
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <Spinner/>}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={genre.image_background}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
