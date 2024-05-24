5652bb98a8894a09501324f237f37026ceb47236
王旭 <wxu743571@gmail.com>
2024-05-24 20:34:19 +0800

    添加Emoji

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import noImage from "../assets/no-image-placeholder.webp";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -13,13 +14,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={game.background_image || noImage} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"} marginTop={3}>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name} <Emoji rating={game.rating_top}/></Heading>
       </CardBody>
     </Card>
   );
