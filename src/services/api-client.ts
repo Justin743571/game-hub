f5c655dc7c33a19644c4b485a0f3792057c85f3b
王旭 <wxu743571@gmail.com>
2024-05-26 20:00:32 +0800

    重构：创建可重用的APIClient

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
-  }
-  
+  count: number;
+  results: T[];
+}
 
-export default axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params:{
-        key:"26c784c8f7b443fa955bc2fb4c190c87"
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "26c784c8f7b443fa955bc2fb4c190c87",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
