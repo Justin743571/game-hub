d3ee025cc41d8bac7e28cab40da9645ce81c7898
王旭 <wxu743571@gmail.com>
2024-05-26 21:56:12 +0800

    重构：简化时间计算

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -30,7 +31,7 @@ const useGames = (gameQuery: GameQuery, pageSize: number) =>
     getNextPageParam: (lastPage, allPage) => {
       return lastPage.next ? allPage.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
   });
 
 export default useGames;
