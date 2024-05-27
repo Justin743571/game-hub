3b005cca2c7dfe277191e33d41e3c36269c0a83a
王旭 <wxu743571@gmail.com>
2024-05-27 21:42:38 +0800

    重构：移动实体接口

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,14 +1,13 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms, { Platform } from "../hooks/usePlatforms";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../store";
 
-
-
 const PlatformSelector = () => {
-  const platformId = useGameQueryStore(s=> s.gameQuery.platformId);
-  const setPlatformId = useGameQueryStore(s=> s.setPlatformId);
+  const platformId = useGameQueryStore((s) => s.gameQuery.platformId);
+  const setPlatformId = useGameQueryStore((s) => s.setPlatformId);
   const { data, error } = usePlatforms();
   const selectedPlatform = usePlatform(platformId);
 
