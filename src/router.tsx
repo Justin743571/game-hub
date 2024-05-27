33ead9398dff6ab4f264603a2a732d71b0c2f228
王旭 <wxu743571@gmail.com>
2024-05-27 20:46:19 +0800

    添加错误页面

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import GameDetail from "./pages/GameDetail";
 import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement:<ErrorPage/>,
     children: [
       { path: "", element: <HomePage /> },
       { path: "games/:id", element: <GameDetail /> },
