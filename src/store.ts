e8e2064b571610b19d5e993b4ab67b0a692d7cf6
王旭 <wxu743571@gmail.com>
2024-05-27 17:29:27 +0800

    修复store文件问题

--- src/store.ts
+++ src/store.ts
@@ -19,12 +19,11 @@ const useGameQueryStore = create<GameQueryStore>((set) => ({
   gameQuery: {},
   setSearchText: (searchText) => set(() => ({ gameQuery: { searchText } })),
   setGenreId: (genreId) =>
-    set((store) => ({ gameQuery: { ...store, genreId } })),
+    set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
   setPlatformId: (platformId) =>
-    set((store) => ({ gameQuery: { ...store, platformId } })),
+    set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
   setSortOrder: (sortOrder) =>
-    set((store) => ({ gameQuery: { ...store, sortOrder } })),
+    set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
 }));
-
 
 export default useGameQueryStore;
