c25652bc30d3870a261989a4114217b11789755b
王旭 <wxu743571@gmail.com>
2024-05-28 10:32:20 +0800

    修复导航栏

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,14 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import loge from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  
   return (
     <HStack padding="10px">
-      <Image src={loge} boxSize="60px" />
+      <Link to='/' >
+        <Image src={loge} boxSize="60px" objectFit='cover' />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
