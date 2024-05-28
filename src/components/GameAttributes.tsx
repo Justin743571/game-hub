6316ac46ac73bab1de902777790e16f15a3633e7
王旭 <wxu743571@gmail.com>
2024-05-28 10:01:17 +0800

    创建游戏预告片

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -7,7 +7,7 @@ interface Props {
   game: Game;
 }
 
-const GameAttibutes = ({ game }: Props) => {
+const GameAttributes = ({ game }: Props) => {
   return (
     <SimpleGrid columns={2} as="dl">
       <DefinitionItem term="Platforms">
@@ -32,4 +32,4 @@ const GameAttibutes = ({ game }: Props) => {
   );
 };
 
-export default GameAttibutes;
+export default GameAttributes;
